d10ee7bd87485f27eb292d87277bc6447bc5ca08
tefuirZ <63311404+tefuirZ@users.noreply.github.com>
2024-06-30 16:06:34 +0800

    Update main.py
    将ini配置文件方式移除，使用数据库存储配置等信息

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
+import os
+import sqlite3
 import urllib
 from time import sleep
 import requests
-import configparser
-import os
 import logging
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-import concurrent.futures
 import sys
-
-
-# 配置日志记录器
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# 创建配置解析器
-config = configparser.ConfigParser()
-
-# 指定以 UTF-8 编码方式读取配置文件
-with open('config.ini', 'r', encoding='utf-8') as configfile:
-    config.read_file(configfile)
-
-# 使用 get 方法从配置文件中获取配置值。第一个参数是段名，第二个参数是键名。
-root_path = config.get('DEFAULT', 'RootPath', fallback="/path/to/root")
-site_url = config.get('DEFAULT', 'SiteUrl', fallback='www.tefuir0829.cn')
-target_directory = config.get('DEFAULT', 'TargetDirectory', fallback='E:\\cloud\\')
-ignored_directories_str = config.get('DEFAULT', 'IgnoredDirectories', fallback='')
-ignored_directories = [d.strip() for d in ignored_directories_str.split(',') if d.strip()]
-token = config.get('DEFAULT', 'Token')  # 从配置文件读取固定的Token
-
-api_base_url = site_url + "/api"
-UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"
-
-traversed_paths = []
+from pathlib import Path
 
 # 配置日志记录器
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# 获取脚本文件所在的目录
+base_dir = Path(__file__).resolve().parent
+
+# 确保数据库路径存在
+db_dir = base_dir / 'instance'
+db_dir.mkdir(parents=True, exist_ok=True)
+
+# 数据库连接
+db_path = db_dir / 'config.db'
+db_path_str = str(db_path)  # 将 Path 对象转换为字符串
+logger.debug(f"数据库路径: {db_path_str}")  # 添加调试信息
+
+conn = sqlite3.connect(db_path_str)
+cursor = conn.cursor()
+
+# 创建配置表
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS config (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    root_path TEXT NOT NULL,
+    site_url TEXT NOT NULL,
+    target_directory TEXT NOT NULL,
+    ignored_directories TEXT,
+    token TEXT NOT NULL,
+    update_existing INTEGER NOT NULL
+)
+''')
+conn.commit()
 
 def requests_retry_session(
         retries=3,
@@ -58,8 +62,7 @@ def requests_retry_session(
     session.mount('https://', adapter)
     return session
 
-
-def list_directory(path):
+def list_directory(path, api_base_url, token, user_agent):
     url_list = api_base_url + "/fs/list"
     payload_list = {
         "path": path,
@@ -70,15 +73,21 @@ def list_directory(path):
     }
     headers_list = {
         'Authorization': token,
-        'User-Agent': UserAgent,
+        'User-Agent': user_agent,
         'Content-Type': 'application/json'
     }
     response_list = requests_retry_session().post(url_list, headers=headers_list, json=payload_list)
-    return response_list.json()
+    response_list.raise_for_status()  # 确保请求成功
 
+    try:
+        return response_list.json()
+    except requests.exceptions.JSONDecodeError as e:
+        logger.error(f"JSON解析错误: {e}")
+        logger.error(f"响应内容: {response_list.text}")
+        return {}
 
-def traverse_directory(path, json_structure, base_url, target_directory, is_root=True):
-    directory_info = list_directory(path)
+def traverse_directory(path, json_structure, base_url, api_base_url, target_directory, token, user_agent, root_path, ignored_directories, is_root=True):
+    directory_info = list_directory(path, api_base_url, token, user_agent)
     if directory_info.get('data') and directory_info['data'].get('content'):
         for item in directory_info['data']['content']:
             if item['name'] in ignored_directories:
@@ -88,13 +97,10 @@ def traverse_directory(path, json_structure, base_url, target_directory, is_root
             if item['is_dir']:
                 new_path = os.path.join(path, item['name'])
                 sleep(5)  # 为了避免请求过快被服务器限制
-                if new_path in traversed_paths:
-                    continue
-                traversed_paths.append(new_path)
                 new_json_object = {}
                 json_structure[item['name']] = new_json_object
-                traverse_directory(new_path, new_json_object, base_url, target_directory, is_root=False)
-            elif is_video_file(item['name']):
+                traverse_directory(new_path, new_json_object, base_url, api_base_url, target_directory, token, user_agent, root_path, ignored_directories, is_root=False)
+            elif is_video_file(item['name']) or is_subtitle_file(item['name']):
                 json_structure[item['name']] = {
                     'type': 'file',
                     'size': item['size'],
@@ -102,97 +108,116 @@ def traverse_directory(path, json_structure, base_url, target_directory, is_root
                 }
 
     if not is_root:  # 如果不是根目录，表示已达到末端，开始写入 .strm 文件
-        create_strm_files(json_structure, target_directory, base_url,
-                          path.replace(root_path, '').strip('/').replace('/', os.sep))
-
+        relative_path = Path(path).relative_to(root_path).as_posix()  # 获取相对于 root_path 的相对路径，并转换为 POSIX 风格
+        create_strm_files(json_structure, target_directory, base_url, relative_path)
 
 def is_video_file(filename):
-    video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv']  # 添加更多的视频格式
+    video_extensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.ts']  # 添加更多的视频格式
     return any(filename.lower().endswith(ext) for ext in video_extensions)
 
+def is_subtitle_file(filename):
+    subtitle_extensions = ['.srt', '.ass', '.ssa', '.vtt']  # 添加更多的字幕格式
+    return any(filename.lower().endswith(ext) for ext in subtitle_extensions)
 
 def create_strm_files(json_structure, target_directory, base_url, current_path=''):
     for name, item in json_structure.items():
-        full_path = os.path.join(target_directory, current_path)
+        full_path = Path(target_directory) / current_path
         if isinstance(item, dict) and item.get('type') == 'file':
             if not item.get('created'):
-                strm_filename = name.rsplit('.', 1)[0] + '.strm'
-                strm_path = os.path.join(full_path, strm_filename)
+                if is_video_file(name):
+                    strm_filename = name.rsplit('.', 1)[0] + '.strm'
+                    strm_path = full_path / strm_filename
 
-                # 检查是否已存在同名的.strm文件
-                if os.path.exists(strm_path):
-                    logger.info(f"{strm_path} 已存在，跳过创建。")
-                    continue
+                    # 检查是否已存在同名的.strm文件
+                    if strm_path.exists():
+                        logger.info(f"{strm_path} 已存在，跳过创建。")
+                        continue
 
-                # 确保目录存在
-                os.makedirs(full_path, exist_ok=True)
-                encoded_file_path = urllib.parse.quote(os.path.join(current_path.replace('\\', '/'), name))
-                video_url = base_url + encoded_file_path
-                item['created'] = True
-                with open(strm_path, 'w', encoding='utf-8') as strm_file:
-                    strm_file.write(video_url)
-                    logger.info(f"{strm_path} 已创建。")
+                    # 确保目录存在
+                    full_path.mkdir(parents=True, exist_ok=True)
+                    encoded_file_path = urllib.parse.quote((Path(current_path) / name).as_posix())
+                    video_url = base_url + encoded_file_path
+                    item['created'] = True
+                    with open(strm_path, 'w', encoding='utf-8') as strm_file:
+                        strm_file.write(video_url)
+                        logger.info(f"{strm_path} 已创建。")
+
+                    # 下载字幕文件
+                    download_subtitle_files(json_structure, name, base_url, full_path, current_path)
         elif isinstance(item, dict):  # 如果是一个目录，递归处理
-            new_directory = os.path.join(full_path, name)
-            os.makedirs(new_directory, exist_ok=True)
-            create_strm_files(item, target_directory, base_url, os.path.join(current_path, name))
+            new_directory = full_path / name
+            new_directory.mkdir(parents=True, exist_ok=True)
+            create_strm_files(item, target_directory, base_url, (Path(current_path) / name).as_posix())
 
+def download_subtitle_files(json_structure, video_name, base_url, full_path, current_path):
+    base_name = video_name.rsplit('.', 1)[0]
+    for name, item in json_structure.items():
+        if is_subtitle_file(name) and name.startswith(base_name):
+            subtitle_path = full_path / name
 
-def process_config(config_file):
-    # 创建配置解析器
-    config = configparser.ConfigParser()
+            # 检查是否已存在同名的字幕文件
+            if subtitle_path.exists():
+                logger.info(f"{subtitle_path} 已存在，跳过下载。")
+                continue
 
-    # 使用 get 方法从配置文件中获取配置值。第一个参数是段名，第二个参数是键名。
-    with open(config_file, 'r', encoding='utf-8') as configfile:
-        config.read_file(configfile)
+            encoded_file_path = urllib.parse.quote((Path(current_path) / name).as_posix())
+            subtitle_url = base_url + encoded_file_path
 
-    # 从配置文件中读取配置信息
-    root_path = config.get('DEFAULT', 'RootPath', fallback="/path/to/root")
-    site_url = config.get('DEFAULT', 'SiteUrl', fallback='www.tefuir0829.cn')
-    target_directory = config.get('DEFAULT', 'TargetDirectory', fallback='E:\\cloud\\')
-    ignored_directories_str = config.get('DEFAULT', 'IgnoredDirectories', fallback='')
+            response = requests_retry_session().get(subtitle_url)
+            response.raise_for_status()
+
+            with open(subtitle_path, 'wb') as subtitle_file:
+                subtitle_file.write(response.content)
+                logger.info(f"{subtitle_path} 已下载。")
+
+def process_config(config_id):
+    # 从数据库中读取配置信息
+    cursor.execute('SELECT * FROM config WHERE id=?', (config_id,))  # 确保表名是正确的
+    config = cursor.fetchone()
+
+    if config is None:
+        logger.error(f"未找到ID为{config_id}的配置")
+        return
+
+    id, name, root_path, site_url, target_directory, ignored_directories_str, token, update_existing = config
     ignored_directories = [d.strip() for d in ignored_directories_str.split(',') if d.strip()]
-    token = config.get('DEFAULT', 'Token')  # 从配置文件读取固定的Token
-
-    # 其他全局变量和函数的定义...
+    logger.info(f"正在运行配置文件：{name} ")
+    # 打印目标目录路径以进行调试
+    logger.info(f"目标目录路径: {target_directory}")
 
     # 初始化JSON结构体并进行目录遍历
     json_structure = {}
 
     # 组装基础URL用于访问视频文件
     base_url = site_url + '/d' + root_path + '/'
+    api_base_url = site_url + '/api'
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0"
 
-    traverse_directory(root_path, json_structure, base_url, target_directory)
+    traverse_directory(root_path, json_structure, base_url, api_base_url, target_directory, token, user_agent, root_path, ignored_directories)
 
-    # 修改后的目标文件夹路径
-    os.makedirs(target_directory, exist_ok=True)  # 确保目标文件夹存在
+    # 确保目标文件夹存在
+    Path(target_directory).mkdir(parents=True, exist_ok=True)
 
     # 创建 .strm 文件
     create_strm_files(json_structure, target_directory, base_url)
 
-    logger.info(f"配置文件 {config_file} 下的strm文件已经创建完成")
+    logger.info(f"配置 {name} 下的strm文件已经创建完成")
 
 def main():
-    global ignored_directories
     logger.info('脚本运行中。。。。。。。')
 
-    # 从命令行参数获取配置文件列表
-    config_files = sys.argv[1:]
-
-    # 检查是否提供了配置文件
-    if not config_files:
-        logger.error("请提供至少一个配置文件路径作为命令行参数")
+    # 从命令行参数获取配置ID
+    if len(sys.argv) < 2:
+        logger.error("请提供配置ID作为命令行参数")
         return
 
-    # 并发处理每个配置文件
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(process_config, config_files)
+    config_id = sys.argv[1]
+    # 处理配置
+    process_config(config_id)
 
     logger.info('所有strm文件创建完成')
     logger.info('热知识：')
-    logger.info('strm文件可以直接在tmm挂削哦')
-    logger.info('感谢使用，使用中有任何问题欢迎留言')
-    logger.info('博客地址：www.tefuir0829.cn')
+    logger.info('')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
